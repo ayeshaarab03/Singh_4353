204cdb0112a7e82e413acaf59b8860e4f3edd0e2
ayeshaarab03 <84541298+ayeshaarab03@users.noreply.github.com>
2023-04-02 23:42:02 -0500

    Update registration.js

--- registration.js
+++ registration.js
@@ -24,11 +24,42 @@ isValid = true;
         return isValid;
       }
 
-      function submit(isValid){
+      function encrypt(password){
+        const password = 'my_password';
+        const hash = crypto.subtle.digest('SHA-256', new TextEncoder().encode(password));
+
+        hash.then(function(result) {
+          const hashed_password = Array.from(new Uint8Array(result)).map(b => b.toString(16).padStart(2, '0')).join('');
+        });
+        return hashed_password;
+      }
+
+      const mysql = require('mysql');
+
+      function submit(isValid, email, password){
         if(isValid){
-          // If information is valid, display a success message
-          alert("Profile saved successfully!");
-          return true;
+          // Connect to the database
+          const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'password',
+            database: 'db'
+          });
+        
+          // Insert the profile information into the database
+          const query = `INSERT INTO UserCredentials VALUES ('${email}', '${encrypt(password)}')`;
+          connection.query(query, function (error, results, fields) {
+            if (error) {
+              console.error(error);
+              alert("An error occurred while saving your profile");
+              return false;
+            }
+            // If the query was successful, display a success message
+            alert("Profile saved successfully!");
+            return true;
+          });
+          // Close the database connection
+          connection.end();
         }
         return false;
       }
